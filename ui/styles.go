d793c783e0833c365fe605d6d43899b7e6ae0693
NStefan002 <n.stefan002@gmail.com>
2025-07-25 14:31:23 +0200

    feat(styles): new better-looking styles

--- ui/styles.go
+++ ui/styles.go
@@ -8,26 +8,64 @@ import (
 
 var (
 	baseStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#DADADA")).
+			Background(lipgloss.Color("#1E1E1E")).
+			Padding(0, 1)
+
+	headerStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFFFF")).
+			Background(lipgloss.Color("#3A3A3A")).
+			Padding(1, 2).
+			Bold(true).
+			Align(lipgloss.Center)
+
+	selectedDateStyle = baseStyle.
+				Foreground(lipgloss.Color("#1E1E1E")).
+				Background(lipgloss.Color("#FFD700")).
+				Bold(true)
+
+	todayStyle = baseStyle.
+			Background(lipgloss.Color("#005F87")).
+			Bold(true)
+
+	dateWithEventStyle = baseStyle.
+				Foreground(lipgloss.Color("#F0F0F0")).
+				Background(lipgloss.Color("#444444")).
+				Bold(true)
+
+	eventHeaderStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("#FFFFFF")).
+				Background(lipgloss.Color("#005F5F")).
+				Bold(true).
+				Padding(0, 1)
+
+	eventStyle = baseStyle.
+			Foreground(lipgloss.Color("#C0C0C0"))
+
+	eventDetailsStyle = lipgloss.NewStyle().
+				Padding(1, 2).
+				Border(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color("#5F5F5F")).
+				Width(50)
+
+	eventListStyle = baseStyle.
 			Padding(0, 1).
-		// Border(lipgloss.NormalBorder()).
-		// BorderForeground(lipgloss.Color("240")).
-		Background(lipgloss.Color("235"))
+			Border(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("#3A3A3A"))
 
-	headerStyle = baseStyle.Background(lipgloss.Color("39")).Bold(true).Align(lipgloss.Center)
+	eventListSelectedStyle = eventListStyle.
+				Background(lipgloss.Color("#5F00AF")).
+				Foreground(lipgloss.Color("#FFFFFF")).
+				Bold(true)
 
-	selectedDateStyle = baseStyle.Background(lipgloss.Color("45")).Bold(true)
+	activeTextinputStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("#FFFFFF")).
+				Background(lipgloss.Color("#005F87")).
+				Padding(0, 1).
+				Bold(true)
 
-	todayStyle = baseStyle.Background(lipgloss.Color("21")).Bold(true)
-
-	dateWithEventStyle = baseStyle.Background(lipgloss.Color("57")).Foreground(lipgloss.Color("230")).Bold(true)
-
-	eventHeaderStyle = baseStyle.Background(lipgloss.Color("33")).Bold(true)
-
-	eventStyle = baseStyle.Foreground(lipgloss.Color("252"))
-
-	eventDetailsStyle = baseStyle.Padding(0, 1).Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240")).Width(50)
-
-	eventListStyle = baseStyle.Padding(0, 1).Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
-
-	eventListSelectedStyle = eventListStyle.Background(lipgloss.Color("57")).Foreground(lipgloss.Color("230")).Bold(true)
+	inactiveTextinputStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("#888888")).
+				Background(lipgloss.Color("#1E1E1E")).
+				Padding(0, 1)
 )
